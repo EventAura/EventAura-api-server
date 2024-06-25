93b070c0b28dbef815f9ba57e7055dae539138ba
Varun Harinath <varunharinath15@gmail.com>
2024-06-25 23:20:36 +0530

    update on participants

--- controllers/ParticipantsController.js
+++ controllers/ParticipantsController.js
@@ -22,7 +22,9 @@ const getParticipantById = async (req, res) => {
 const getParticipantsByEvent = async (req, res) => {
   try {
     const eventId = await EventModel.findById(req.params.id);
-    const users = await User.find({ eventName: eventId.eventName });
+    const users = await User.find({ eventName: eventId.eventName }).sort({
+      userRegistrationDate: -1,
+    });
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
