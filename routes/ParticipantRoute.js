bf5a7665f0e4f36be0df4608d3d712c6f3aa1cde
Varun Harinath <varunharinath15@gmail.com>
2024-12-06 23:30:40 +0530

    Update to the checkin server

--- routes/ParticipantRoute.js
+++ routes/ParticipantRoute.js
@@ -2,6 +2,8 @@ import {
   getParticipants,
   getParticipantById,
   getParticipantsByEvent,
+  getUserEntryStatus,
+  updateUserEntryStatus,
 } from "../controllers/ParticipantsController.js";
 import express from "express";
 
@@ -10,5 +12,7 @@ const ParticipantRouter = express.Router();
 ParticipantRouter.get("/participants", getParticipants);
 ParticipantRouter.get("/participant/:id", getParticipantById);
 ParticipantRouter.get("/participants/event/:id", getParticipantsByEvent);
+ParticipantRouter.get("/participant/status/:id", getUserEntryStatus);
+ParticipantRouter.patch("/participant/updateStatus/:id", updateUserEntryStatus);
 
 export default ParticipantRouter;
